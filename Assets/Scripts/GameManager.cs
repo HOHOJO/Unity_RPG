c262a1eb81c437b8cf873fe03dcc14f81d359e9c
HOHOJO <ckwhs95@gmail.com>
2023-09-06 20:46:41 +0900

    캐릭터 변경 1차

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Text NameText;
     public InputField inputName;
     public Button btn;
+    public GameObject ch_1;
+    public GameObject ch_2;
+    public GameObject select;
+    public GameObject Main_1;
+    public GameObject Main_2;
+    public GameObject Main;
 
     // Start is called before the first frame update
     void Start()
@@ -26,4 +33,16 @@ public class GameManager : MonoBehaviour
         NameText.text = inputName.text;
     }
 
+    public void selectImage(int i)
+    {
+        if(i == 1)
+        {
+            select = ch_1;
+        }
+        else
+        {
+            select = ch_2;
+        }
+
+    }
 }
